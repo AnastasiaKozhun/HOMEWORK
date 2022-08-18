572d8fbce517d987b558542c6d8f4d05fea58ce3
AnastasiaKozhun <110128264+AnastasiaKozhun@users.noreply.github.com>
2022-08-18 14:02:03 +0300

    commit

---
+++ hw/src/by/itacademy/hw5/Task1/Person.java
@@ -0,0 +1,23 @@
+package by.itacademy.hw5.Task1;
+
+public class Person {
+    public String fullName;
+    public int age;
+
+    public Person() {
+    }
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public void move(){
+        System.out.printf("Передвигается %s", this.fullName);
+        System.out.println();
+    }
+    public void talk() {
+        System.out.printf("Говорит %s", this.fullName);
+        System.out.println();
+    }
+}
